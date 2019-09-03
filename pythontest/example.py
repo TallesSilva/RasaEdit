15c52f684675bc3fc38e529dafd6ac2b1065cca9
TallesSilva <tallesbugs@hotmail.com>
2019-09-03 18:04:16 -0300

    Comit de correçoes nos campos do supplier 03/09/2019 - 18:04

--- pythontest/example.py
+++ pythontest/example.py
@@ -11,13 +11,15 @@ Col_User = db.User
 Col_Supplier = db.Supplier
 Col_Timetable = db.Timetable
 Col_Metadata = db.Metadata
+Col_Timetable_Date = db.Timetable
+Col_Timetable_None = db.Timetable
+Col_Confirmation = db.Confirmed
 
 Id = fake.bban()
 CPF = fake.cpf()
 phone = fake.phone_number()
-name = fale.name()
+name = fake.name()
 address = fake.address()
-data = fake.data()
 now = datetime.now()
 
 Col_User.insert_one({
@@ -31,20 +33,33 @@ Col_User.insert_one({
 
 Col_Supplier.insert_one({
     'Id' : CPF,
-    'Name': phone,
-    'Workhours': name,
-    'when' : address,
+    'Name': name,
+    'Workhours': None, #descobrir como preencher
+    'when' : None,
+    'available' : None,
 })
 
-Col_Timetable.insert_one({
-    'Id' : Id,
-    'IDSupplier': IDSupplier,
-    'IDUser': IDUser,
+Col_Timetable_Date.insert_one({
+    'Id' : Id, #descobrir como linkar
+    'IDSupplier': None, #descobrir como linkar
+    'IDUser': None, #descobrir como linkar
     'date' : now.strftime("%d/%m/%Y %H:%M:%S"),
 })
 
-Col_Timetable.insert_one({
+Col_Timetable_None.insert_one({
+    'Id' : Id, #descobrir como linkar
+    'IDSupplier': None, #descobrir como linkar
+    'IDUser': None, #descobrir como linkar
+    'date' : None,
+})
 
+Col_Confirmation.insert_one({
+    'Id' : Id,
+    'IDtimetable' : None, #descobrir como linkar
+    'Confirmed' : None,
+    'rescheduled' : None,
+    'rescheduled' : None,
+    'when' : None,
 })
 
 Col_Metadata.insert_one({
