7f8b5e6a86f7e7b55d0715f394298344301bcbf5
TallesSilva <tallesbugs@hotmail.com>
2019-09-10 18:49:19 -0300

    10/09/2019 - 18:48 -> Criação da main com token e credencial, insert ainda nao funcionou ( talvez o host não permite )

--- pythontest/fakes.py
+++ pythontest/fakes.py
@@ -36,40 +36,78 @@ def get_fake_supplier():
 
 def get_fake_customer():
     payload_customer = {
-    "nome": fake.name(),
-    "cpf": fake.cpf(),
-    "dia_preferencia": None,
-    "hora_preferencia": None,
-    "empresa": None,
-    "endereco": {
-        "rua": fake.street_name(),
-        "numero": fake.building_number(),
-        "complemento": None,
-        "bairro": fake.bairro(),
-        "cep": fake.postcode(),
-        "cidade": fake.city(),
-        "estado": fake.estado_sigla(),
-        "latitude": None,
-        "longitude": None
-    },
-    "contato": {
-        "fixo": fake.cellphone_number(),
-        "celular": fake.cellphone_number(),
-        "email": "tallesr@kyros.com.br",
-        "site": None,
-        "whatsapp": fake.cellphone_number(),
-        "telegram": fake.cellphone_number()
+        "nome": fake.name(),
+        "cpf": fake.cpf(),
+        "dia_preferencia": None, #faker.providers.date_time
+        "hora_preferencia": None,
+        "empresa": None,
+        "endereco": {
+            "rua": fake.street_name(),
+            "numero": fake.building_number(),
+            "complemento": None,
+            "bairro": fake.bairro(),
+            "cep": fake.postcode(),
+            "cidade": fake.city(),
+            "estado": fake.estado_sigla(),
+            "latitude": None,
+            "longitude": None
+        },
+        "contato": {
+            "fixo": fake.cellphone_number(),
+            "celular": fake.cellphone_number(),
+            "email": "tallesr@kyros.com.br",
+            "site": None,
+            "whatsapp": fake.cellphone_number(),
+            "telegram": fake.cellphone_number()
+        }
     }
-}
     return payload_customer
 
-def get_fake_timetable_none():
+def get_fake_timetable_none(): #como linkar com dados já existentes ?
     payload_timetable = {
-    "IDSupplier" : "",
-    "IDUser" : "",
-    "date" :   ""   #( d/m/a, h:m:s) none
+    "data": None,
+    "status": None,
+    "observacao": "",
+    "task": None,
+    "supplier": None,
+    "customer": None,
+    "company": None
     }
     return payload_timetable
 
-def get_fake_timetable_date():
-    raise NotImplementedError
+def get_fake_timetable_date(): #como linkar com dados já existentes ?
+    payload_timetable = {
+    "data": None,
+    "status": None,
+    "observacao": "",
+    "task": None,
+    "supplier": null,
+    "customer": null,
+    "company": null
+    }
+    return payload_timetable
+
+def get_fake_date():
+    payload_date =  {
+        'summary': summary,
+        'location': location,
+        'description': description,
+        'start': {
+            'dateTime': start_time.strftime("%Y-%m-%dT%H:%M:%S"),
+            'timeZone': timezone,
+        },
+        'end': {
+            'dateTime': end_time.strftime("%Y-%m-%dT%H:%M:%S"),
+            'timeZone': timezone,
+        },
+        'attendees': [
+        {'email':attendees },
+    ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 10},
+            ],
+        },
+    }
