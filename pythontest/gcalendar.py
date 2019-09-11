c737a3043f81f115f12bd7c11e093841660b4b7c
TallesSilva <tallesbugs@hotmail.com>
2019-09-11 17:45:42 -0300

    11/09/2019 - 17:45 -> Commit de segurança

--- pythontest/gcalendar.py
+++ pythontest/gcalendar.py
@@ -1,19 +1,32 @@
-from apiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
+from __future__ import print_function
+import datetime
 import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
-scopes = ['https://www.googleapis.com/auth/calendar']
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 def config():
     creds = None
-    service = None
-    flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
-    credentials = flow.run_console()
-    pickle.dump(credentials, open("token.pkl", "wb"))
-    credentials = pickle.load(open("token.pkl", "rb"))
-    service = build("calendar", "v3", credentials=credentials)
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+        service = build('calendar', 'v3', credentials=creds)
 
 def insert_datetime(y,m,d,th,tm,ts,td):
+    #chamar os parametros creds e services da função de configuraçao
     start_time = datetime(y, m, d, th, tm, ts)
     end_time = start_time + timedelta(hours=td)
     timezone = 'Asia/Kolkata'
@@ -39,6 +52,5 @@ def insert_datetime(y,m,d,th,tm,ts,td):
     }  
     print('event created')
     return service.events().insert(calendarId='primary', body=event).execute()
-
 
 config()
